7afbbc3c6d3ce83416d320e2ec924db2ab03197e
fede.caceres <fede.caceres@fd5cb99e-d79a-11de-89a3-1386611daa76>
2009-12-15 01:59:08 +0000

    Ya aparecen de manera randómicamente probabilística los obstáculos.

--- vrp/src/game_entity_renderer.c
+++ vrp/src/game_entity_renderer.c
@@ -16,9 +16,27 @@ void game_entity_render_obstacle(struct game_entity * entity, int elapsed_time) 
     calcular_coordenadas(entity->pos, &a);
 
     glPushMatrix();
-    glColor4f(1.0, 0.0, 0.0, calcular_alpha(a.z));
     glTranslatef(a.x, a.y, a.z); 
-    glutSolidCone(0.25, 0.5, 16, 2);
+
+    switch (entity->instance) {
+        case GAME_ENTITY_INSTANCE_OBSTACLE_PYR:
+            glRotatef(-90, 1,0,0);
+            glRotatef(45, 0,0,1);
+            glColor4f(1.0, 0.0, 0.0, calcular_alpha(a.z));
+            glutWireCone(0.65, 1.0, 4, 1);
+            break;
+        case GAME_ENTITY_INSTANCE_OBSTACLE_CUBE:
+            glTranslatef(0.0, 0.5, 0.0);
+            glColor4f(0.0, 1.0, 0.0, calcular_alpha(a.z));
+            glutSolidCube(1.4);
+            break;
+        case GAME_ENTITY_INSTANCE_OBSTACLE_CIL:
+            glRotatef(-90, 1,0,0);
+            glTranslatef(0.0, 0.5, 0.0);
+            glColor4f(1.0, 1.0, 0.0, calcular_alpha(a.z));
+            glutSolidCylinder(0.3, 3.5, 8, 2);
+            break;
+    }
     glPopMatrix();
 
 }
