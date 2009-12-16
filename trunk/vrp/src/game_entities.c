477adbb5f455d54d12c9ec18ed7a9c4db18d0b18
fede.caceres <fede.caceres@fd5cb99e-d79a-11de-89a3-1386611daa76>
2009-12-16 14:29:32 +0000

    * UNTESTED * Seteado de los bounds de la entidad.

--- trunk/vrp/src/game_entities.c
+++ trunk/vrp/src/game_entities.c
@@ -52,7 +52,10 @@ struct game_entity *  borrar_de_lista(struct game_entity * entity) {
     return next; // retornar el siguiente para que la función anterior encadene
 }
 
-int game_entity_obstacle_instance() {
+/**
+ * Elige una nueva entidad de manera probabilística
+ */
+int pick_game_entity_obstacle_instance() {
     float probability = rand()/(float)RAND_MAX;
     
     if (probability > .85)
@@ -61,6 +64,41 @@ int game_entity_obstacle_instance() {
         return GAME_ENTITY_INSTANCE_OBSTACLE_CUBE;
     else
         return GAME_ENTITY_INSTANCE_OBSTACLE_PYR;
+}
+
+/**
+ * Setea los bounds de la entidad de acuerdo a su tipo
+ */
+void set_game_entity_bounds(struct game_entity *) {
+	switch(game_entity->instance) {
+		case GAME_ENTITY_INSTANCE_OBSTACLE_PYR;
+			//glutWireCone(0.65, 1.0, 4, 1);
+			game_entity->bound_min.x = - 0.65/2;
+			game_entity->bound_min.y =   0;
+			game_entity->bound_min.z = - 0.65/2;
+			game_entity->bound_max.x = + 0.65/2;
+			game_entity->bound_max.y = + 1;
+			game_entity->bound_max.z = + 0.65/2;
+			break;
+		case GAME_ENTITY_INSTANCE_OBSTACLE_CUBE;
+			//-1 +1
+			game_entity->bound_min.x = - 1;
+			game_entity->bound_min.y = - 1;
+			game_entity->bound_min.z = - 1;
+			game_entity->bound_max.x = + 1;
+			game_entity->bound_max.y = + 1;
+			game_entity->bound_max.z = + 1;
+			break;
+		case GAME_ENTITY_INSTANCE_OBSTACLE_CIL:
+			//glutSolidCylinder(0.2, 3.2, 8, 1);
+			game_entity->bound_min.x = - 0.2/2;
+			game_entity->bound_min.y =   0;
+			game_entity->bound_min.z = - 0.2/2;
+			game_entity->bound_max.x = + 0.2/2;
+			game_entity->bound_max.y = + 3.2;
+			game_entity->bound_max.z = + 0.2/2;
+			break;
+	}
 }
 
 /**
@@ -73,13 +111,14 @@ struct game_entity *  create_entity() {
     nuevo = (struct game_entity *)malloc(sizeof(struct game_entity));
     
     nuevo->type = GAME_ENTITY_TYPE_OBSTACLE;
-    nuevo->instance = game_entity_obstacle_instance(); //GAME_ENTITY_INSTANCE_OBSTACLE_CIL;
+    nuevo->instance = pick_game_entity_obstacle_instance(); //GAME_ENTITY_INSTANCE_OBSTACLE_CIL;
     nuevo->pos.x = (rand()/(float)RAND_MAX)* (ROAD_WIDTH-0.3)*2 - (ROAD_WIDTH-0.3);
     nuevo->pos.y = 1;
     nuevo->pos.z = -ROAD_MAX;
     nuevo->dissapear_at = 0;
     nuevo->renderer = game_entity_render_obstacle;
     nuevo->next = NULL;
-
+	// setear los bounds
+	set_game_entity_bounds(nuevo);
     return nuevo;
 }
