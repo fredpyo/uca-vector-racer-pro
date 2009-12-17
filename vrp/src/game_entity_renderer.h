1cd7f5b03e502a974652897590a461b1aa9c3aa4
fede.caceres <fede.caceres@fd5cb99e-d79a-11de-89a3-1386611daa76>
2009-12-17 23:42:33 +0000

    Implementados - Pausa - Lights off con el teclado - Coin

--- vrp/src/game_entity_renderer.h
+++ vrp/src/game_entity_renderer.h
@@ -1,2 +1,3 @@
 void game_entity_render_obstacle(struct game_entity * game_entity, int elapsed_time);
 void game_entity_render_powerup(struct game_entity * entity, int elapsed_time);
+void game_entity_render_coin(struct game_entity * entity, int elapsed_time);
