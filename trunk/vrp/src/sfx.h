141644e381eee4b059cccdfac66482093d2c0a7b
fede.caceres <fede.caceres@fd5cb99e-d79a-11de-89a3-1386611daa76>
2009-12-18 13:28:27 +0000

    We've got sound!

--- trunk/vrp/src/sfx.h
+++ trunk/vrp/src/sfx.h
@@ -2,7 +2,26 @@
 ** SFX.c PUBLIC
 **
 */
+#define SFX_BOOM1 0
+#define SFX_BOOM2 1
+#define SFX_POWERUP 3
+#define SFX_POWERUP_GOOD 4
+#define SFX_POWERUP_BAD 5
+
+#define SFX_SONG_INTRO 0
+#define SFX_SONG_TITLE 1
+#define SFX_SONG_RANKING 2
+#define SFX_SONG_RANKING_WIN 3
+#define SFX_SONG_GAME_OVER 4
+#define SFX_SONG_GAME1 5
+#define SFX_SONG_GAME2 6
+#define SFX_SONG_GAME3 7
+#define SFX_SONG_GAME4 8
+#define SFX_SONG_GAME5 9
+
+
 void sfx_init();
 
 void music_play(char * filename);
 void music_stop(int fade);
+void sfx_play_sample(int sample);
